5c02b35b38e5dd58be0ddf864554d01c7689e542
heritierbagumire <heritierbagumire367@gmail.com>
2025-05-26 21:46:12 +0200

    backend removed

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,16 +1,19 @@
 import { Stack } from 'expo-router';
 import { useEffect } from 'react';
 import { useAuthStore } from '@/store/auth-store';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function RootLayout() {
   const { isAuthenticated } = useAuthStore();
 
   return (
-    <Stack screenOptions={{ headerShown: false }}>
+    <SafeAreaView>
+      <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="(auth)" options={{ headerShown: false }} />
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       <Stack.Screen name="transaction/new" options={{ presentation: 'modal', title: 'New Transaction' }} />
       <Stack.Screen name="transaction/[id]" options={{ title: 'Transaction Details' }} />
-    </Stack>
+     </Stack>
+    </SafeAreaView>
   );
 }
